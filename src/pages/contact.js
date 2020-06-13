ef09498a6c7faf1cfdd4d86f2ae7456772aeffb2
tyrelh <krankyjoe@gmail.com>
2020-06-13 09:14:05 -0700

    gatsby starter project

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default function Contact() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Link to="/">Home</Link>
+      <Header>Get in touch!</Header>
+      <p>Send us a message!</p>
+    </div>
+  )
+}
