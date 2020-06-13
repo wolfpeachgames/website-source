57650bbd91b836d335dc7406658fa2a9da7b0343
tyrelh <krankyjoe@gmail.com>
2020-06-13 13:35:44 -0700

    gatsby tutorial - create container component

---
+++ src/components/container.js
@@ -0,0 +1,10 @@
+import React from "react"
+import containerStyles from "./container.module.css"
+
+export default function Container({ children }) {
+  return (
+    <div className={containerStyles.container}>
+        {children}
+    </div>
+  )
+}
