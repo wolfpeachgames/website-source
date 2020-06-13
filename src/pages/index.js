67bfb2e44f9e0534700284e9b2ce0725f27cb7ba
tyrelh <krankyjoe@gmail.com>
2020-06-13 13:51:02 -0700

    use Container component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import Header from "../components/header"
+import Container from "../components/container"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+      
+    <Container>
       {/* <Link to="/contact/">Contact</Link> */}
       <Header>🐺🍑🎮</Header>
       <p>Coming soon...</p>
-    </div>
+    </Container>
   )
 }
