fa31cafe0a5e600b46a5baeaa5d749f3145be1d7
tyrelh <krankyjoe@gmail.com>
2020-06-13 13:09:33 -0700

    placeholder content

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,9 @@ import Header from "../components/header"
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <Header>Wolfpeach Games</Header>
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      {/* <Link to="/contact/">Contact</Link> */}
+      <Header>🐺🍑🎮</Header>
+      <p>Coming soon...</p>
     </div>
   )
 }
