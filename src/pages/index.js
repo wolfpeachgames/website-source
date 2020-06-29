7aae5b1afd3f2bcf5ca52d07b283f2363d8c7a67
tyrelh <krankyjoe@gmail.com>
2020-06-28 23:05:19 -0700

    add new fonts and make wordmark image width dynamic

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       
     <Container>
       {/* <Link to="/contact/">Contact</Link> */}
-      <Header><img src="https://raw.githubusercontent.com/wolfpeachgames/website-source/master/src/images/wordmark2.png" alt="Wolf Peach Games word mark"/></Header>
+      <Header><img className="headerWordmark" src="https://raw.githubusercontent.com/wolfpeachgames/website-source/master/src/images/wordmark2.png" alt="Wolf Peach Games word mark"/></Header>
       <h2>Coming soon...</h2>
       <p>Wolfpeach Games is <a href="http://theverdigris.ca">Stephanie</a> and <a href="https://tyrelh.github.io">Tyrel</a></p>
       <p>🐺🍑🎮</p>
